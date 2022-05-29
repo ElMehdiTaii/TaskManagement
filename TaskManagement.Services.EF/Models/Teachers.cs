3fb897139eeeac4b56f1c70678c4f6b5154d28fa
taiielmehdi <t.mehdi@peaqock.com>
2022-05-29 01:02:38 +0100

    Adding Ef Models

---
+++ TaskManagement.Services.EF/Models/Teachers.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagement.Services.EF.Models
+{
+    public class Teachers
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? MainSubjectTeaching { get; set; }
+    }
+}
